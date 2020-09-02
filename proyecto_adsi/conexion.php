1630aaf8db2d9274b8eb1bfe529e14e78d87d06a
Hans Malcolm <hansmalcolm1@gmail.com>
2020-09-02 18:19:02 -0500

    empty project

--- proyecto_adsi/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$user="root";
-$password="";
-$host="localhost";
-$db_name="proyecto_adsi";
-$con= new PDO("mysql:host=$host;dbname=$db_name", $user, $password);
-
-?>
--- proyecto_adsi/index.php
+++
@@ -1,9 +0,0 @@
-<DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-</head>
-<body>
-	<button><a href="ListarEstudiantesDB.php">Estudiantes</a></button>
-</body>
-</html>
